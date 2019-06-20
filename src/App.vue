e547ddee03813ac9982bb66b89a438db0773591f
liunian03 <2364052432@qq.com>
2019-06-20 17:51:13 +0800

    ca

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
     <div id="app">
         <!--<img src="./assets/logo.png">-->
-        <v-header :sellers="sellers"></v-header>
-        <div class="content">
-            <div class="tab-item">
-                <router-link to="/goods">商品</router-link>
-            </div>
-            <div class="tab-item">
-                <router-link to="/ratings">评论</router-link>
-            </div>
-            <div class="tab-item">
-                <router-link to="/sellers">商家</router-link>
-            </div>
-        </div>
-        <router-view></router-view>
+        <v-header></v-header>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
 
-    import header from './components/header/header';
+    import header from './components/header';
 
     export default {
         data() {
             return {
-                sellers: {}
+            
             }
         },
         // created(){//钩子
@@ -38,14 +26,15 @@
         // },
         name: 'App',
         components: {
-            'v-header': header,
+            'v-header':header
         }
+        
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
     @import "./common/stylus/mixin.styl"
-    #app {
+    #app
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
@@ -53,33 +42,5 @@
         color: #2c3e50;
         /*padding: 0.15rem;*/
         font-size:0.28rem;
-    }
-
-    #app .content {
-        display: flex;
-        width: 100%;
-        height: 0.8rem;
-        line-height: 0.8rem;
-        border-1px(rgba(7, 17, 27, 0.1));
-    }
-
-    #app .content .tab-item {
-        flex: 1;
-        text-align: center;
-    }
-
-    #app .content .tab-item a {
-        display: block;
-        text-decoration: none;
-        color: #333;
-    }
-
-    a:active {
-        background: red;
-    }
-
-    #app .content .tab-item a.active {
-        background: none;
-        color: rgb(240, 20, 20);
-    }
+    
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="test">
+        {{'2018'| filterA | filterB}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "headerNav",
+        methods:{
+        
+        },
+        filters:{
+            filterA:function(value){
+                return value + "年"
+            },
+            filterB:function(value){
+                return value + 'hello word'
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
